57b14e9067573662ac459569c4dc22b0cd21cfbc
Nahar <nahartewatia19@gmail.com>
2023-11-25 18:06:40 +0530

    commit

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [counter,setCounter] = useState(0);
+
+  const addValue = () => {
+    if(counter<20){
+    setCounter(counter+1);
+    }
+  }
+
+  const removeValue = () =>{
+    if(counter>=1){
+    setCounter(counter-1);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>COUNTER APP {counter}</h1>
+      <h2>i have used usestate hook to mannage state of count to display on ui at several positions</h2>
+      <br />
+      <button onClick={addValue}
+      >add value {counter}</button>
+      <br />
+      <button onClick={removeValue}
+      >remove value {counter}</button>
     </>
   )
 }
---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='bg-green-400 text-black p-4 rounded-lg'>learning tailwind</h1>
+     <Card userName="nahar" btnTxt="click me"/>
+     <Card userName="singh"/>
+    </>
+  )
+}
+
+export default App
